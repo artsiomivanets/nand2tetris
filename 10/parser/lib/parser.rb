bf98392f553e3f6de0cfc82fbf2e2322cef75110
artsiomivanets <artem.ivanec96@gmail.com>
2023-08-18 23:57:31 +0300

    simple tokenizer

--- 10/parser/lib/parser.rb
+++ 10/parser/lib/parser.rb
@@ -5,31 +5,41 @@ require_relative "parser/version"
 require "pry"
 
 module Tokenizer
+  KEYWORDS = %w[class construction method field static
+                var int char boolean void true false null this
+                let do if else while return]
+
   def self.process(c, acc)
     case acc[:state]
-    when "start"
+    when "common"
       return acc if [" ", "\n"].include?(c)
 
       return process(c, acc.merge(state: "comment")) if c == "/"
+
+      return process(c, acc.merge(state: "symbol")) if %w[) ( } {].include?(c)
 
       process(c, acc.merge(state: "word"))
     when "comment"
       return acc unless c == "\n"
 
-      process(c, acc.merge(state: "start"))
+      process(c, acc.merge(state: "common"))
+    when "symbol"
+      word = { type: :symbol, value: c }
+      acc.merge({ state: "common", word: "", words: acc[:words] + [word] })
     when "word"
-      return acc unless %w[) ( } {].include?(c)
-
-      word = acc[:word] + c
-      words = acc[:words].push(word)
-      acc.merge(word: "", words: words)
-
+      if c.match(/\w/)
+        acc.merge(word: acc[:word] + c)
+      else
+        type = KEYWORDS.include?(acc[:word]) ? :keyword : :identifier
+        word = { type: type, value: acc[:word] }
+        acc.merge(state: "common", word: "", words: acc[:words] + [word])
+      end
     end
   end
 
   def self.call(context)
     result = context[:symbols].split("").reduce({
-                                                  state: "start",
+                                                  state: "common",
                                                   words: [],
                                                   word: ""
                                                 }) do |acc, c|
